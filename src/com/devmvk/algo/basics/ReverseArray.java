24206063eeb5dee23c49f49ae0d46e1b2eff9b10
Varun Kumar Madupu <developer.varunkumar@gmail.com>
2022-01-30 11:22:51 +0530

    adds ReverseArray logic.

--- src/com/devmvk/algo/basics/ReverseArray.java
+++ src/com/devmvk/algo/basics/ReverseArray.java
@@ -1,4 +1,22 @@
 package com.devmvk.algo.basics;
 
 public class ReverseArray {
+
+    /*
+    * Reverse {array} with size n
+    *
+    * Algo approach
+    * when n is even/odd iterate (n/2 - 1) times and swap array[i] with array[n - i - 1]
+    *
+    * */
+    public static int[] reverse(int[] array){
+        int temp;
+        int arraySize = array.length;
+        for (int i = 0; i < arraySize; i++) {
+            temp = array[i];
+            array[i] = array[arraySize - i - 1];
+            array[arraySize - i - 1] = temp;
+        }
+        return array;
+    }
 }
