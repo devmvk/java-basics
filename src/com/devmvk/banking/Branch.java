87db0c7a048d56f86b65f7bdc8f1bfd407f6e02e
Varun Kumar Madupu <developer.varunkumar@gmail.com>
2022-01-31 22:15:38 +0530

    develops branch class

--- src/com/devmvk/banking/Branch.java
+++ src/com/devmvk/banking/Branch.java
@@ -1,2 +1,22 @@
-package com.devmvk.banking;public class Branch {
+package com.devmvk.banking;
+
+import java.util.ArrayList;
+
+public class Branch {
+
+    private ArrayList<Customer> customers;
+
+    public Branch() {
+        this.customers = new ArrayList<Customer>();
+    }
+
+    public ArrayList<Customer> getCustomers() {
+        return customers;
+    }
+
+    public void addCustomer(String customerName, double initialTransaction){
+        Customer customer = new Customer(customerName);
+        customer.addTransaction(initialTransaction);
+        getCustomers().add(customer);
+    }
 }
