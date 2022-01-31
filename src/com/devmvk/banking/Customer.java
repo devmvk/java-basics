d7da30c7ea24d13574f2013e76790208bf69adde
Varun Kumar Madupu <developer.varunkumar@gmail.com>
2022-01-31 22:06:34 +0530

    develops customer class

--- src/com/devmvk/banking/Customer.java
+++ src/com/devmvk/banking/Customer.java
@@ -1,2 +1,31 @@
-package com.devmvk.banking;public class Customer {
+package com.devmvk.banking;
+
+import java.util.ArrayList;
+
+public class Customer {
+
+    private final String name;
+    private ArrayList<Double> transactions;
+
+    public Customer(String name) {
+        this.name = name;
+        this.transactions = new ArrayList<Double>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Double> getTransactions() {
+        return transactions;
+    }
+
+    public void setTransactions(ArrayList<Double> transactions) {
+        this.transactions = transactions;
+    }
+
+    public void addTransaction(double value){
+        this.transactions.add(value);
+    }
+
 }
