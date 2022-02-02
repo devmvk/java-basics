0b7432be94de70d53662a40acddc34eb6387e068
Varun Kumar Madupu <developer.varunkumar@gmail.com>
2022-02-02 23:49:47 +0530

    creates custom linked list functionality

--- src/com/devmvk/ds/CustomLinkedList.java
+++ src/com/devmvk/ds/CustomLinkedList.java
@@ -1,2 +1,23 @@
-package com.devmvk.ds;public class CustomLinkedList {
+package com.devmvk.ds;
+
+public class CustomLinkedList<T> {
+
+    private CustomLinkedList<T> link;
+    private T value;
+
+    public CustomLinkedList<T> getLink() {
+        return link;
+    }
+
+    public void setLink(CustomLinkedList<T> link) {
+        this.link = link;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
 }
