4885d76d388953a45108fb0e3e7983f140a6417f
Varun Kumar Madupu <developer.varunkumar@gmail.com>
2022-02-08 23:35:51 +0530

    provides songs to album

--- src/com/devmvk/music/Album.java
+++ src/com/devmvk/music/Album.java
@@ -1,4 +1,16 @@
 package com.devmvk.music;
 
+import java.util.LinkedList;
+
 public class Album {
+
+    private LinkedList<Song> songs;
+
+    public LinkedList<Song> getSongs() {
+        return songs;
+    }
+
+    public void setSongs(LinkedList<Song> songs) {
+        this.songs = songs;
+    }
 }
