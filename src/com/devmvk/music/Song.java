72317142665bb197efb70b429fa0fa6be183973d
Varun Kumar Madupu <developer.varunkumar@gmail.com>
2022-02-08 23:34:24 +0530

    creates song pojo class

--- src/com/devmvk/music/Song.java
+++ src/com/devmvk/music/Song.java
@@ -1,2 +1,19 @@
-package com.devmvk.music;public class Song {
+package com.devmvk.music;
+
+public class Song {
+    private final String name;
+    private final Integer durationInSeconds;
+
+    public Song(String name, Integer durationInSeconds) {
+        this.name = name;
+        this.durationInSeconds = durationInSeconds;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getDurationInSeconds() {
+        return durationInSeconds;
+    }
 }
